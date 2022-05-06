6000fe65973bae1d5014a5308aa2b426460bb812
wangzhang <745268436@qq.com>
2022-05-06 14:20:47 +0800

    [update] listNode

---
+++ data_structure/linkedList/solution/hasCycle.go
@@ -0,0 +1,19 @@
+package solution
+
+import "algorithm-pattern/data_structure/linkedList"
+
+func hasCycle(head *linkedList.ListNode) bool {
+	if head == nil {
+		return false
+	}
+	slow := head
+	fast := head.Next
+	for fast != nil && fast.Next != nil {
+		if slow == fast {
+			return true
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return false
+}
