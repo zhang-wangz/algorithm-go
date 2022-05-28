5d3faacc52798dcc66639238fd11bc71a73ce3c6
wangzhang <745268436@qq.com>
2022-05-28 13:17:53 +0800

    [update] 5-28-2022

--- leetcode/main/main.go
+++ leetcode/main/main.go
@@ -1,6 +1,14 @@
 package main
 
+import (
+	"algorithm-pattern/leetcode/solution"
+	"fmt"
+)
+
 func main() {
 	//data := [][]int{{4, 2, 3}, {0, 0, 1}, {7, 6, 5}}
 	//solution.CutOffTree(data)
+	s := "(()())(())"
+	res := solution.RemoveOuterParentheses(s)
+	fmt.Println(res)
 }
