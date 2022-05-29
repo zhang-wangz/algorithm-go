a2049360a76bf203fdeec241a91722e481a17d5d
wangzhang <745268436@qq.com>
2022-05-29 12:39:18 +0800

    [update] 5-29-2022

--- leetcode/main/main.go
+++ leetcode/main/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	//data := [][]int{{4, 2, 3}, {0, 0, 1}, {7, 6, 5}}
 	//solution.CutOffTree(data)
-	s := "(()())(())"
-	res := solution.RemoveOuterParentheses(s)
+	s := "2001:0db8:85a3:00000:0:8A2E:0370:7334"
+	res := solution.ValidIPAddress(s)
 	fmt.Println(res)
 }
---
+++ leetcode/solution/5-29-validIPAddress.go
@@ -0,0 +1,62 @@
+package solution
+
+import (
+	"strings"
+)
+
+// https://leetcode.cn/problems/validate-ip-address/
+// 验证ip4或者ip6
+
+func ValidIPAddress(queryIP string) string {
+	str := strings.Split(queryIP, ".")
+	f := false
+	if len(str) == 1 {
+		str = strings.Split(queryIP, ":")
+		f = true
+	}
+	// ipv4
+	if !f {
+		if len(str) != 4 {
+			return "Neither"
+		}
+		for _, item := range str {
+			num := 0
+			if item == "" {
+				return "Neither"
+			}
+			for i := 0; i < len(item); i++ {
+				// 防止首位为0
+				if i == 0 && item[i] == '0' && len(item) != 1 {
+					return "Neither"
+				}
+				if item[i] >= '0' && item[i] <= '9' {
+					num = num*10 + int(item[i]-'0')
+				} else {
+					return "Neither"
+				}
+			}
+			if num < 0 || num > 255 {
+				return "Neither"
+			}
+		}
+		return "IPv4"
+	} else {
+		if len(str) != 8 {
+			return "Neither"
+		}
+		for _, item := range str {
+			if len(item) > 4 || len(item) < 1 {
+				return "Neither"
+			}
+			for i := 0; i < len(item); i++ {
+				c := item[i]
+				if (c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F') {
+					continue
+				} else {
+					return "Neither"
+				}
+			}
+		}
+		return "IPv6"
+	}
+}
