c6bb11230702ee5417c3a96e7e105a39d8e206bd
wangzhang <745268436@qq.com>
2022-06-06 09:35:13 +0800

    [update] 6-5

---
+++ leetcode/solution/6-5-RandPoint.go
@@ -0,0 +1,35 @@
+package solution
+
+import (
+	"math"
+	"math/rand"
+)
+
+// https://leetcode.cn/problems/generate-random-point-in-a-circle/
+// 随机生成圆内一个点
+type Solution struct {
+	radius float64
+	x, y   float64
+}
+
+func Constructor1(radius float64, x_center float64, y_center float64) Solution {
+	return Solution{radius: radius, x: x_center, y: y_center}
+}
+
+func (this *Solution) RandPoint() []float64 {
+	// sina^2 + cosa^2 = 1
+	for {
+		x := rand.Float64()*2 - 1
+		y := rand.Float64()*2 - 1 // -1 1
+		if x*x+y*y <= 1 {
+			return []float64{this.radius*x + this.x, this.radius*y + this.y}
+		}
+	}
+}
+
+func (s *Solution) RandPoint2() []float64 {
+	// 为满足等概率
+	s.radius = math.Sqrt(rand.Float64() * s.radius * s.radius)
+	sin, cos := math.Sincos(rand.Float64() * 2 * math.Pi)
+	return []float64{s.radius*cos + s.x, s.radius*sin + s.y}
+}
---
+++ leetcode/solution/6-6-Book.go
@@ -0,0 +1,13 @@
+package solution
+
+// https://leetcode.cn/problems/my-calendar-iii/
+type MyCalendarThree struct {
+}
+
+func Constructor() MyCalendarThree {
+
+}
+
+func (this *MyCalendarThree) Book(start int, end int) int {
+
+}
