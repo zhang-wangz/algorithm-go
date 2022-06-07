a5743834509d586953164a3777765086edacd940
wangzhang <745268436@qq.com>
2022-06-07 11:12:53 +0800

    [update] 二叉搜索朴素思想

---
+++ binarySearchTree/solution/deleteNode.go
@@ -0,0 +1,29 @@
+package solution
+
+import "algorithm-pattern/data_structure/binaryTree"
+
+func deleteNode(root *binaryTree.Node, key int) *binaryTree.Node {
+	if root == nil {
+		return root
+	}
+
+	if root.Val > key {
+		root.Left = deleteNode(root.Left, key)
+	} else if root.Val < key {
+		root.Right = deleteNode(root.Right, key)
+	} else if root.Val == key {
+		if root.Left == nil {
+			return root.Right
+		} else if root.Right == nil {
+			return root.Left
+		} else {
+			cur := root.Right
+			for cur.Left != nil {
+				cur = cur.Left
+			}
+			cur.Left = root.Left
+			return root.Right
+		}
+	}
+	return root
+}
