8b3d5eaf5888ab30eeadf9c84770cb561687443f
wangzhang <745268436@qq.com>
2022-06-11 23:22:13 +0800

    更新 6-11 lcs和lis的关系

--- leetcode/solution/6-11-minFlipsMonoIncr.go
+++ leetcode/solution/6-11-minFlipsMonoIncr.go
@@ -1,5 +1,7 @@
 package solution
 
+import "math"
+
 // https://leetcode.cn/problems/flip-string-to-monotone-increasing/
 // 如果一个二进制字符串，是以一些 0（可能没有 0）后面跟着一些 1（也可能没有 1）的形式组成的，那么该字符串是 单调递增 的。
 // 给你一个二进制字符串 s，你可以将任何 0 翻转为 1 或者将 1 翻转为 0 。
@@ -15,6 +17,56 @@ func MinFlipsMonoIncr(s string) int {
 		dp[i][1] = min(dp[i-1][0], dp[i-1][1]) + (int(s[i-1]-'0') ^ 1)
 	}
 	return min(dp[len(s)][0], dp[len(s)][1])
+}
+
+// lcs 和 lis
+func MinFlipsMonoIncr2(s string) int {
+	Max := 0
+	g := make([]int, len(s)+2)
+	for i := 0; i < len(g); i++ {
+		g[i] = len(s) + 100
+	}
+	for i := 0; i < len(s); i++ {
+		// [1,i+1)
+		t := int(s[i] - '0')
+		l, r := 1, i+1
+		for l < r {
+			mid := l + (r-l)>>1
+			if g[mid] <= t {
+				l = mid + 1
+			} else {
+				r = mid
+			}
+		}
+		// 找出所有的序列，获得其中最长的
+		g[r] = t
+		Max = max(Max, r)
+	}
+	return len(s) - Max
+}
+
+// 前缀和
+func MinFlipsMonoIncr3(s string) int {
+	sum := make([]int, len(s)+1)
+	ans := math.MaxInt
+	for i := 1; i <= len(s); i++ {
+		sum[i] = sum[i-1] + int(s[i-1]-'0')
+	}
+	for i := 1; i <= len(s); i++ {
+		// 左边有多少1 要改成0
+		l := sum[i-1]
+		r := (len(s) - i) - (sum[len(s)] - sum[i])
+		ans = min(ans, l+r)
+	}
+	return ans
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	} else {
+		return a
+	}
 }
 
 func min(a, b int) int {
