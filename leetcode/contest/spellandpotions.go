180d6a7a11a776186465b5c15047c2b6b8f6fb11
wangzhang <745268436@qq.com>
2022-06-12 10:43:55 +0800

    更新 contest2

--- leetcode/contest/spellandpotions.go
+++ leetcode/contest/spellandpotions.go
@@ -7,21 +7,37 @@ func spellsAndpotions(spells []int, potions []int, success int64) []int {
 	sort.Ints(potions)
 	for i := 0; i < len(spells); i++ {
 		l := 0
-		r := len(potions) - 1
-		// 找第一个 >= success的值
-		for l+1 < r {
+		r := len(potions)
+		// 找 >= success的最小下标
+		for l < r {
 			mid := l + (r-l)/2
 			if potions[mid]*spells[i] >= int(success) {
 				r = mid
 			} else {
-				l = mid
+				l = mid + 1
 			}
 		}
-		if potions[l]*spells[i] >= int(success) {
-			res = append(res, l)
-		} else if potions[r]*spells[i] >= int(success) {
-			res = append(res, r)
+		if l == len(potions) {
+			res = append(res, 0)
+		} else {
+			res = append(res, len(potions)-l)
 		}
+		// 或者
+		//for l+1 < r {
+		//	mid := l + (r-l)/2
+		//	if potions[mid]*spells[i] >= int(success) {
+		//		r = mid
+		//	} else {
+		//		l = mid
+		//	}
+		//}
+		//if potions[l]*spells[i] >= int(success) {
+		//	res = append(res, len(potions)-l)
+		//} else if potions[r]*spells[i] >= int(success) {
+		//	res = append(res, len(potions)-r)
+		//}else {
+		//	res = append(res, 0)
+		//}
 
 	}
 	return res
