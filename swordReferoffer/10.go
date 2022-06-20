445d6bb02cb414a6e111653377dd414f5f1796cd
wangzhang <745268436@qq.com>
2022-06-20 17:59:37 +0800

    update offer

---
+++ swordReferoffer/10.go
@@ -0,0 +1,8 @@
+package swordReferoffer
+
+// 给定一个整数数组和一个整数 k ，请找到该数组中和为 k 的连续子数组的个数。
+// 因为数据范围包括负数，所以不能用滑动窗口
+
+func subarraySum(nums []int, k int) int {
+
+}
--- swordReferoffer/8.go
+++ swordReferoffer/8.go
@@ -1,5 +1,31 @@
 package swordReferoffer
 
-//func minSubArrayLen(target int, nums []int) int {
-//
-//}
+func minSubArrayLen(target int, nums []int) int {
+	left := 0
+	right := 0
+	sum := 0
+	minV := 1<<31 - 1
+	for right < len(nums) {
+		t := nums[right]
+		right++
+		sum += t
+
+		for sum >= target {
+			minV = min(right-left, minV)
+			t := nums[left]
+			left++
+			sum -= t
+		}
+	}
+	if minV == 1<<31-1 {
+		return 0
+	}
+	return minV
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
