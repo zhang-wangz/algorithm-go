1037a0b050af644201fa4e503ce37287b0b6526d
wangzhang <745268436@qq.com>
2022-07-01 11:30:00 +0800

    fix err

--- leetcode/solution/7-1-diffWaysToCompute.go
+++ leetcode/solution/7-1-diffWaysToCompute.go
@@ -5,15 +5,15 @@ import "strconv"
 // https://leetcode.cn/problems/different-ways-to-add-parentheses/
 // 分治
 
-func diffWaysToCompute(expression string) (ans []int) {
+func DiffWaysToCompute(expression string) (ans []int) {
 	if expression == "" || len(expression) == 0 {
 		return ans
 	}
 	for i := 0; i < len(expression); i++ {
 		if expression[i] == '+' || expression[i] == '-' || expression[i] == '*' {
 			opr := expression[i]
-			left := diffWaysToCompute(expression[:i])
-			right := diffWaysToCompute(expression[i+1:])
+			left := DiffWaysToCompute(expression[:i])
+			right := DiffWaysToCompute(expression[i+1:])
 			for _, l := range left {
 				for _, r := range right {
 					if opr == '+' {
