86970fe82800789427c6aa7348aa4182c8d4cb10
wangzhang <745268436@qq.com>
2022-07-12 09:02:06 +0800

    update day 7-12-2022

---
+++ leetcode/solution/7-12-oddCells.go
@@ -0,0 +1,21 @@
+package solution
+
+//https://leetcode.cn/problems/cells-with-odd-values-in-a-matrix/
+
+func oddCells(m int, n int, indices [][]int) int {
+	r := map[int]int{}
+	c := map[int]int{}
+	for _, v := range indices {
+		r[v[0]]++
+		c[v[1]]++
+	}
+	ans := 0
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if (r[i]+c[j])&1 == 1 {
+				ans++
+			}
+		}
+	}
+	return ans
+}
