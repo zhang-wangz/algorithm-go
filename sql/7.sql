3b74447dc7eafc707f812f240e2fb3a769da576c
wangzhang <745268436@qq.com>
2022-07-14 11:29:08 +0800

    update sql

--- sql/7.sql
+++ sql/7.sql
@@ -1,7 +1,7 @@
 
 
 DELETE p1 from Person p1, Person p2
-where p1.email = p2.email and p1.id > p2.id
+where p1.email = p2.email and p1.id > p2.id;
 
 # Please write a DELETE statement and DO NOT write a SELECT statement.
 # Write your MySQL query statement below
