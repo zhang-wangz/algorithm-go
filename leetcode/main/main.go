7fd258c560425a38714de50df597eb78d0d3c933
wangzhang <745268436@qq.com>
2022-07-22 10:22:07 +0800

    update day 7-22-2022

--- leetcode/main/main.go
+++ leetcode/main/main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	//q := make([]int, 2, 9)
+	q := make([][]int, 2)
+	//q[1] = append(q[1], 2)
+	fmt.Println(len(q[1]))
 }
---
+++ leetcode/solution/7-22-intersectionSizeTwo.go
@@ -0,0 +1,30 @@
+package solution
+
+import "sort"
+
+// https://leetcode.cn/problems/set-intersection-size-at-least-two/
+// 先把区间进行排序，然后从后往前逐一进行判断，如果符合交集个数则跳过，否则从该区间取数字加入交集
+func intersectionSizeTwo(intervals [][]int) int {
+	n := len(intervals)
+	sort.Slice(intervals, func(i, j int) bool {
+		if intervals[i][0] == intervals[j][0] {
+			return intervals[i][1] > intervals[j][1]
+		} else {
+			return intervals[i][0] < intervals[j][0]
+		}
+	})
+	ans := 0
+	// 交集定义
+	vals := make([][]int, n)
+	m := 2
+	for i := n - 1; i >= 0; i-- {
+		for j, k := intervals[i][0], len(vals[i]); k < m; k++ {
+			ans++
+			for p := i - 1; p >= 0 && intervals[p][1] >= j; p-- {
+				vals[p] = append(vals[p], j)
+			}
+			j++
+		}
+	}
+	return ans
+}
