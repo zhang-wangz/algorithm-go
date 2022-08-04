ecf09c5618b89301fdc0d1aaa316f86fb68e6f6f
wangzhang <745268436@qq.com>
2022-08-04 17:54:23 +0800

    update day 2022-8-4

---
+++ leetcode/solution/2022-8-4.go
@@ -0,0 +1,20 @@
+package main
+
+import "sort"
+
+// https://leetcode.cn/problems/minimum-subsequence-in-non-increasing-order/
+func minSubsequence(nums []int) []int {
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(nums)))
+	tot := 0
+	for i, num := range nums {
+		tot += num
+		if tot > sum-tot {
+			return nums[:i+1]
+		}
+	}
+	return []int{}
+}
