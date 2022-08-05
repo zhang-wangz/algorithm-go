6431f1efbe9e51fde1f10d639091d1e48c448cc9
wangzhang <745268436@qq.com>
2022-08-05 10:00:24 +0800

    update tusen2

---
+++ leetcode/other/tusen/2/1.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func main() {
+	var s string
+	fmt.Scanln(&s)
+	f := false
+	cnt := 0
+	for _, v := range s {
+		if unicode.IsDigit(rune(v)) {
+			if !f {
+				f = true
+				cnt++
+			}
+		} else {
+			f = false
+		}
+	}
+	fmt.Println(cnt)
+}
---
+++ leetcode/other/tusen/2/2.go
@@ -0,0 +1,5 @@
+package main
+
+func findLUSlength(strs []string) int {
+	return 0
+}
