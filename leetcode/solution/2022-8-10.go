6dafee91639a3c1beb13380f6118c7b626131255
wangzhang <745268436@qq.com>
2022-08-10 10:01:55 +0800

    update day 2022-8-10

---
+++ leetcode/solution/2022-8-10.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"strconv"
+	"unicode"
+)
+
+func main() {
+	solveEquation("x+5-3+x=6+x-2")
+}
+func solveEquation(equation string) string {
+	fac, val := 0, 0
+	sign := 1 // 左边默认为正数
+	for i := 0; i < len(equation); {
+		// 右边默认为负数
+		if equation[i] == '=' {
+			sign = -1
+			i++
+		}
+
+		s := sign
+		if equation[i] == '+' {
+			i++
+		} else if equation[i] == '-' {
+			s = -s
+			i++
+		}
+		num := 0
+		valid := false
+		for ; i < len(equation) && unicode.IsDigit(rune(equation[i])); i++ {
+			valid = true
+			num = num*10 + int(equation[i]-'0')
+		}
+		if valid {
+			s *= num
+		}
+		if i < len(equation) && equation[i] == 'x' {
+			fac += s
+			i++
+		} else {
+			// 如果不是x，就在val上加上，因为不是系数
+			val += s
+		}
+	}
+	if fac == 0 {
+		if val == 0 {
+			return "Infinite solutions"
+		}
+		return "No solution"
+	}
+	if val%fac != 0 {
+		return "No solution"
+	}
+	return "x=" + strconv.Itoa(-val/fac)
+}
--- leetcode/solution/main.go
+++ leetcode/solution/main.go
@@ -1,10 +1,10 @@
 package main
 
-func main() {
-	root := &TreeNode{Val: 1}
-	root.Left = &TreeNode{Val: 2}
-	root.Right = &TreeNode{Val: 3}
-	root.Left.Left = &TreeNode{Val: 4}
-	//root.Left.Right = &TreeNode{Val:1}
-	addOneRow(root, 5, 4)
-}
+//func main() {
+//	root := &TreeNode{Val: 1}
+//	root.Left = &TreeNode{Val: 2}
+//	root.Right = &TreeNode{Val: 3}
+//	root.Left.Left = &TreeNode{Val: 4}
+//	//root.Left.Right = &TreeNode{Val:1}
+//	addOneRow(root, 5, 4)
+//}
