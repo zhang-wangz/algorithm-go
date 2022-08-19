9438c3710e8f8c5a851a98e3ebdd59b7a8650261
wangzhang <745268436@qq.com>
2022-08-19 09:38:36 +0800

    update day 2022-8-19

---
+++ leetcode/solution/2022-8-19.go
@@ -0,0 +1,11 @@
+package main
+
+func busyStudent(startTime []int, endTime []int, queryTime int) int {
+	cnt := 0
+	for i := range startTime {
+		if startTime[i] <= queryTime && queryTime <= endTime[i] {
+			cnt++
+		}
+	}
+	return cnt
+}
