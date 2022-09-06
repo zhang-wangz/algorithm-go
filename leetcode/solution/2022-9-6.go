6902b9b93af8fcec0720cab218cdbd02d5088276
wangzhang <745268436@qq.com>
2022-09-06 09:32:50 +0800

    update day 2022-9-6

---
+++ leetcode/solution/2022-9-6.go
@@ -0,0 +1,15 @@
+package main
+
+func uniqueLetterString(s string) (ans int) {
+	idx := map[rune][]int{}
+	for i, c := range s {
+		idx[c] = append(idx[c], i)
+	}
+	for _, arr := range idx {
+		arr = append(append([]int{-1}, arr...), len(s))
+		for i := 1; i < len(arr)-1; i++ {
+			ans += (arr[i] - arr[i-1]) * (arr[i+1] - arr[i])
+		}
+	}
+	return
+}
--- leetcode/solution/main.go
+++ leetcode/solution/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "sort"
 
+func main() {
+	a := sort.IntSlice{}
+	a = append(a, 2)
+	a = append(a, 1)
+	sort.Sort(a)
+	print(a[0])
 }
