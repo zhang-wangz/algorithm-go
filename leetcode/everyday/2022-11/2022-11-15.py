b10c2f67d90e4587e76f664bd69a84eb043fe520
wangzhang <745268436@qq.com>
2022-11-15 09:26:53 +0800

    update 2022-11-115

---
+++ leetcode/everyday/2022-11/2022-11-15.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        res = 0
+        for num in sorted(boxTypes, key=lambda x:-x[1]):
+            cnt, n = num
+            if truckSize >= cnt:
+                res += cnt * n
+                truckSize -= cnt
+            elif truckSize > 0 and truckSize < cnt:
+                res += truckSize * n
+                truckSize = 0
+            if  truckSize <= 0 : break
+        return res
+            
